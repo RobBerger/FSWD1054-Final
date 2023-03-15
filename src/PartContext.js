c0a3991425a885c9a8625bd1b70949dd8801d8c9
berger <robertberger.dev@gmail.com>
2023-03-15 10:27:02 -0400

    populated PartContext.js with starter code

---
+++ src/PartContext.js
@@ -0,0 +1,63 @@
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const PartContext = createContext()
+
+export const PartProvider = (props) => {
+    const [parts, setParts] = useState([])
+
+    useEffect(() => {
+        async function getParts() {
+            await refreshParts()
+        }
+        getParts()
+    }, []);
+
+    function refreshParts() {
+        return axios.get("http://localhost:3001/parts")
+        .then(response => {
+            setParts(response.data)
+        })
+    }
+
+    function getPart(id) {
+        return axios.get(`http://localhost:3001/parts/${id}`)
+            .then(response => 
+                new Promise((resolve) => resolve(response.data)))
+    }
+
+    function deletePart(id) {
+        axios.delete(`http://localhost:3001/parts/${id}`)
+        .then(refreshParts)
+    }
+
+    function addPart(part) {
+        return axios.post("http://localhost:3001/parts", part)
+        .then(response => {
+            refreshParts()
+            return new Promise((resolve) => resolve(response.data))
+        })
+    }
+
+    function updatePart(part) {
+        return axios.put(`http://localhost:3001/parts/${part.id}`, part)
+        .then(response => {
+            refreshParts()
+            return new Promise((resolve) => resolve(response.data))
+        })
+    }
+
+    return (
+        <PartContext.Provider
+            value={{
+                parts,
+                getPart,
+                deletePart,
+                addPart,
+                updatePart
+            }}
+        >
+            {props.children}
+        </PartContext.Provider>
+    )
+}
