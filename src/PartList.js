a1dc3f803fda1c9acb2f347b9ccb3a11db09907d
berger <robertberger.dev@gmail.com>
2023-03-15 19:13:10 -0400

    stuck

--- src/PartList.js
+++ src/PartList.js
@@ -3,32 +3,30 @@ import ListGroup from 'react-bootstrap/ListGroup'
 import Stack from 'react-bootstrap/Stack'
 import { Link, Outlet } from 'react-router-dom'
 import { PartContext } from './PartContext'
+import CardGroup from 'react-bootstrap/CardGroup'
 
 function PartList(props) {
   function partList(parts) {
     if (parts === null) return
     return parts.map((part) =>
-      <ListGroup.Item key={part.id}>
+      <CardGroup.Item key={part.id}>
         <Link to={`/parts/${part.id}`} key={part.id} >
           {part.partName} {part.partPrice}
         </Link>
-      </ListGroup.Item>
+      </CardGroup.Item>
     )
   }
 
   return (
     <>
     <h1>Parts</h1>
-    <Stack direction="horizontal" gap={3}>
-    <ListGroup className="align-self-start w-75">
+    <CardGroup>
     <PartContext.Consumer>
       {({parts}) => (
         partList(parts)
       )}
     </PartContext.Consumer>
-    </ListGroup>
-    <Outlet />
-    </Stack>
+    </CardGroup>
     </>
   )
 }
