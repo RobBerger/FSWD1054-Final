bd1f4b954cb02cac425350826743ccf0834225b2
berger <robertberger.dev@gmail.com>
2023-03-16 14:38:24 -0400

    improved the card layout

--- src/PartList.js
+++ src/PartList.js
@@ -5,12 +5,14 @@ import { Link, Outlet } from 'react-router-dom'
 import { PartContext } from './PartContext'
 import CardGroup from 'react-bootstrap/CardGroup'
 import Card from 'react-bootstrap/Card'
+import { Row } from 'react-bootstrap'
 
 function PartList(props) {
   function partList(parts) {
     if (parts === null) return
     return parts.map((part) =>
       <Card key={part.id}>
+        <Card.Img variant='top' src={part.imgURL} />
         <Card.Body>
             <Card.Title>{part.partName}</Card.Title>
             <Card.Subtitle>{part.partNumber}</Card.Subtitle>
@@ -22,13 +24,13 @@ function PartList(props) {
   return (
     <>
     <h1>Parts</h1>
-    <CardGroup>
+    <Row xs={1} md={2} lg={3} className="g-4">
     <PartContext.Consumer>
       {({parts}) => (
         partList(parts)
       )}
     </PartContext.Consumer>
-    </CardGroup>
+    </Row>
     </>
   )
 }
