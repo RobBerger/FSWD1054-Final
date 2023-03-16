fc956a5fcddfdd1c47886eaf35a2a7c756ea5581
berger <robertberger.dev@gmail.com>
2023-03-16 09:56:45 -0400

    got cards to render on screen

--- src/PartList.js
+++ src/PartList.js
@@ -4,16 +4,18 @@ import Stack from 'react-bootstrap/Stack'
 import { Link, Outlet } from 'react-router-dom'
 import { PartContext } from './PartContext'
 import CardGroup from 'react-bootstrap/CardGroup'
+import Card from 'react-bootstrap/Card'
 
 function PartList(props) {
   function partList(parts) {
     if (parts === null) return
     return parts.map((part) =>
-      <CardGroup.Item key={part.id}>
-        <Link to={`/parts/${part.id}`} key={part.id} >
-          {part.partName} {part.partPrice}
-        </Link>
-      </CardGroup.Item>
+      <Card key={part.id}>
+        <Card.Body>
+            <Card.Title>{part.partName}</Card.Title>
+            <Card.Subtitle>{part.partNumber}</Card.Subtitle>
+        </Card.Body>
+      </Card>
     )
   }
 
