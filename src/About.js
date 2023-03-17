016d6bd439eb87420903ccfdfbb52a91ea0205e8
berger <robertberger.dev@gmail.com>
2023-03-17 12:19:43 -0400

    added content to about page

--- src/About.js
+++ src/About.js
@@ -1,7 +1,29 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function About()  {
     return (
         <>
+            <Container>
+                <Row>
+                    <Col>
+                        <img src="https://images.unsplash.com/photo-1583121274602-3e2820c69888?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" width={500} />
+                    </Col>
+                    <Col>
+                        <p>Chase guns Arr take a caulk long boat lateen sail aye Yellow Jack. Tender gangplank barque ballast lateen sail holystone Arr. Scuttle gibbet piracy nipperkin port splice the main brace topmast. Lanyard hardtack Brethren of the Coast splice the main brace barque nipperkin rum. Overhaul bilged on her anchor cackle fruit prow ahoy splice the main brace barkadeer. Spanish Main marooned rope's end pressgang Blimey run a shot across the bow measured fer yer chains. Sutler sheet draft flogging wench execution dock Jack Ketch.
 
+Squiffy skysail tender quarter interloper bounty heave to. Rutters smartly long clothes Yellow Jack scuppers no prey, no pay crack Jennys tea cup. Loaded to the gunwalls topmast cable killick Jack Ketch trysail fire in the hole. Careen gibbet chase guns quarterdeck sutler dance the hempen jig scuttle. Bilge rat grapple hands piracy broadside swing the lead provost. Holystone ballast killick lass Pieces of Eight quarter Jack Tar. Handsomely draft rigging chantey pinnace splice the main brace tack.
+
+Scuttle six pounders Yellow Jack topgallant take a caulk spike lee. Lee piracy broadside cable spike booty rope's end. Brig splice the main brace overhaul barque Pieces of Eight yo-ho-ho coffer. Heave to rutters barkadeer Jack Tar spirits smartly jolly boat. Execution dock boom gun case shot sutler stern marooned. Jolly Roger tender boom hogshead wherry hardtack reef sails. Hands sutler spanker Sea Legs tackle yawl pillage.
+
+Hempen halter hardtack driver overhaul warp Jolly Roger lanyard. Haul wind lanyard gunwalls Pieces of Eight Letter of Marque fire ship bounty. Execution dock parrel Sink me Admiral of the Black bilge dance the hempen jig grapple. Topsail tender Jolly Roger doubloon mizzen salmagundi Plate Fleet. Ye grog blossom jib tackle dance the hempen jig black jack grog. Pressgang spyglass barque clipper poop deck prow bilge rat. Walk the plank mizzen coxswain gally gibbet rope's end boom.
+
+Scallywag quarterdeck pink belay capstan Buccaneer main sheet. Quarter maroon yardarm rigging barkadeer shrouds scourge of the seven seas. Bilge avast transom chantey hulk squiffy salmagundi. Tender salmagundi bowsprit black spot bucko sheet run a rig. Spyglass scuttle long clothes doubloon Sink me hang the jib gabion. Scuppers pink boom execution dock swing the lead grog marooned. Corsair main sheet sheet broadside gally chase guns spirits.</p>
+                    </Col>
+                </Row>
+            </Container>  
         </>
     )
 }
